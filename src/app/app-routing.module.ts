727c11359d4a6cfbf7a04906fbc9245de219ab22
Marcela <marcelapereirads@gmail.com>
2021-05-07 19:30:10 -0300

    Home page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
 import { InputPageComponent } from './pages/input-page/input-page.component';
 
 const routes: Routes = [
   { path: 'input', component: InputPageComponent },
-  { path: '', component: InputPageComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from './core/header/header.module';
 import { SidebarMenuModule } from './core/sidebar-menu/sidebar-menu.module';
 import { InputPageModule } from './pages/input-page/input-page.module';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
